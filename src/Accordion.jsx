728a3eccf94c6a0bbcf8ac1755693972d2733623
MuteenK <kundangarmuteen@protonmail.com>
2021-06-03 15:13:43 +0530

    made responsive

--- src/Accordion.jsx
+++ src/Accordion.jsx
@@ -2,12 +2,12 @@ import React, {useState} from 'react';
 
 const Accordion = (props) => {
 
-    const [show, setshow] = useState(false);
+    const [show, setshow] = useState(false);    // hook for setting up accordion state
 
     return (
         <div className="accordion" key={props.data.id}>
             <div className="qPanel">
-                <button className="trigger" onClick = {() => setshow(!show)}>{ (show === false) ? <i className="fa fa-plus"></i> : <i class="fa fa-minus"></i>}</button>
+                <button className="trigger" onClick = {() => setshow(!show)}>{ (show === false) ? <i className="fa fa-plus"></i> : <i className="fa fa-minus"></i>}</button>
                 <h4>{props.data.question}</h4>
             </div>
             {
